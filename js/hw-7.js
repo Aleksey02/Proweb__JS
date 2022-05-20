ba757f78f9ddf6e5bfa0f802d94c65ad0ae56b9a
Aleksey02 <98608292+Aleksey02@users.noreply.github.com>
2022-05-20 10:01:02 +0500

    Create hw-7.js

---
+++ js/hw-7.js
@@ -0,0 +1,5 @@
+userName = prompt('Введите своё имя');
+userYear = +prompt('Введите свой год рождения');
+nowYear = +prompt('Введите текущий год');
+
+alert(userName + ', Ваш возраст ' + (nowYear-userYear));
